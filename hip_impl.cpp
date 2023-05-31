ff56aae7895f3e9b2419de6828cfd41e37b6c3d3
Burlen Loring <bloring@lbl.gov>
2023-05-31 16:20:22 +0000

    separate translation units for OpenMP and HIP amdclang15

---
+++ hip_impl.cpp
@@ -0,0 +1,16 @@
+#define DEFINE_HIP
+#include "hip_impl.h"
+
+INSTANTIATE_HIP(float)
+INSTANTIATE_HIP(double)
+INSTANTIATE_HIP(char)
+INSTANTIATE_HIP(short)
+INSTANTIATE_HIP(int)
+INSTANTIATE_HIP(long)
+INSTANTIATE_HIP(long long)
+INSTANTIATE_HIP(unsigned char)
+INSTANTIATE_HIP(unsigned short)
+INSTANTIATE_HIP(unsigned int)
+INSTANTIATE_HIP(unsigned long)
+INSTANTIATE_HIP(unsigned long long)
+
